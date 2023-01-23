7f844a7fd15ece07bce3107c22ed80cb02caf012
Lucio <lucio.bruno.printes@hotmail.com>
2023-01-23 03:11:17 -0300

    Completo

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
-
 const nome = prompt('Qual o seu nome de usuário?');
 
 const lista = {
     'name':nome
 };
 
-const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",lista);
-promessa.then(respostaChegou);
-promessa.catch(deuRuim);
+function conectando(){
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",lista);
+}
 
 function respostaChegou(resposta){
-    console.log('reposta do servidor chegou');
-    console.log(resposta);
+    
 }
 
-function deuRuim(){
-    console.log('Deu ruim A receita nao foi salva');
+function deuRuim(erro){
+
+    const erroDoUsuario = erro.response.status;
+
+    if(erroDoUsuario === 400){
+        document.location.reload(true);
+    }
 }
+
+conectando();
+
+function statusDoUsuario(){
+    const status = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', lista);
+}
+
+const statusUsuario = setInterval(statusDoUsuario,5000);
+
+buscarMensagem();
 
 function buscarMensagem(){
-    const url = "https://mock-api.driven.com.br/api/v6/uol/messages";
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET",url,false);
-    xhttp.send();
-    console.log(xhttp.responseText);
+
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(mensagensChegaram);
+    promise.catch();
+
 }
 
+let corpoDasMensagens = document.querySelector('.conversas');
+
+function mensagensChegaram(resposta){
+
+    corpoDasMensagens.innerHTML = "";
+
+    let todasMensagens = resposta.data;
+
+    for(let i=0;i<todasMensagens.length;i++){
+        if(todasMensagens[i].text === 'entra na sala...' || todasMensagens[i].text === 'sai da sala...'){
+            corpoDasMensagens.innerHTML = corpoDasMensagens.innerHTML + `<div class ='entrada-saida'><p><span class='horario'>(${todasMensagens[i].time})</span><span class='usuario'>  ${todasMensagens[i].from}</span> para <span class='usuario'>${todasMensagens[i].to}</span>:  <span>${todasMensagens[i].text}</span></p></div>`;            
+        }else if(todasMensagens[i].to === 'Todos'){
+            corpoDasMensagens.innerHTML = corpoDasMensagens.innerHTML + `<div class ='mensagem-todos'><p><span class='horario'>(${todasMensagens[i].time})</span><span class='usuario'>  ${todasMensagens[i].from}</span> para <span class='usuario'>${todasMensagens[i].to}</span>:  <span>${todasMensagens[i].text}</span></p></div>`;
+        }else{
+            corpoDasMensagens.innerHTML = corpoDasMensagens.innerHTML + `<div class ='mensagem-reservada'><p><span class='horario'>(${todasMensagens[i].time})</span><span class='usuario'>  ${todasMensagens[i].from}</span> reservadamente para <span class='usuario'>${todasMensagens[i].to}</span>:  <span>${todasMensagens[i].text}</span></p></div>`;
+        }
+        corpoDasMensagens.lastChild.scrollIntoView(); 
+    }
+
+}
+
+const recarregarMensagens = setInterval(buscarMensagem,mensagensChegaram, 3000);
+
+function deuErroMensagens(erro){
+
+}
 
 let mensagem;
 
@@ -45,8 +84,10 @@ function enviarMensagem(){
     }
 
     const enviarMensagem = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",mensagemEnviada);
-    enviarMensagem.then(respostaChegou);
-    enviarMensagem.catch(deuRuim);
-
+    enviarMensagem.then(deuCertoMensagem);
+    enviarMensagem.catch();
 }
 
+function deuCertoMensagem(){
+    buscarMensagem();
+}
